379f322c0fa2322393d82d5f7ee2fec60005c3f2
rizkyherdiansyah <herdiansyahrizky10@gmail.com>
2019-11-25 11:46:19 +0700

    blog

---
+++ admin/index.php
@@ -0,0 +1,27 @@
+<!--Header-->
+<?php include ('../layouts/includes/head.php'); ?>
+<!--End Header-->
+  <body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+    <!--Navbar-->
+    <?php include ('../layouts/includes/navbar.php'); ?>
+    <!--End Navbar-->
+    <div class="app-body">
+        <!--Sidebar-->
+        <?php include ('../layouts/includes/sidebar.php'); ?>
+        <!--End Sidebar-->
+        <!--Main Content-->
+        <main class="main">
+        </main>
+        <!--End Main Content-->
+ 
+    </div>
+    <!--Footer-->
+    <?php include ('../layouts/includes/footer.php'); ?>
+    <!--End Footer-->
+    <!-- CoreUI and necessary plugins-->
+    <!--Script-->
+    <?php include ('../layouts/includes/scripts.php'); ?>
+    <!--End Script-->
+    
+  </body>
+</html>
---
+++ layouts/includes/sidebar.php
@@ -0,0 +1,22 @@
+<div class="sidebar">
+        <nav class="sidebar-nav">
+          <ul class="nav">
+            <li class="nav-item">
+              <a class="nav-link" href="/admin">
+                <i class="nav-icon icon-speedometer"></i> Home
+                <span class="badge badge-primary">NEW</span>
+              </a>
+            </li>
+            <li class="nav-title">Manajemen Blog</li>
+            <li class="nav-item">
+              <a class="nav-link" href="/kategori">
+                <i class="nav-icon icon-drop"></i>Kategori</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/artikel>
+                <i class="nav-icon icon-pencil"></i>Artikel</a>
+            </li>
+                      </ul>
+        </nav>
+        <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+      </div>
